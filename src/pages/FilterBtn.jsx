8e7393b7a66ccb4a1544f88153ab3edb72bb3460
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2024-08-04 22:07:27 +0530

    <label className="btn btn-outline-primary" htmlFor="">

--- src/pages/FilterBtn.jsx
+++ src/pages/FilterBtn.jsx
@@ -1,21 +1,31 @@
-
 function FilterBtn() {
   return (
     <>
       <div className="form-check">
-  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-  <label className="form-check-label" htmlFor="flexRadioDefault1">
-    Default radio
-  </label>
-</div>
-<div className="form-check">
-  <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
-  <label className="form-check-label" htmlFor="flexRadioDefault2">
-    Default checked radio
-  </label>
-</div>
+        <input
+          className="form-check-input"
+          type="radio"
+          name="flexRadioDefault"
+          id="flexRadioDefault1"
+        />
+        <label class="btn btn-outline-primary" htmlFor="btn-check-outlined">
+          Single toggle
+        </label>
+      </div>
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="radio"
+          name="flexRadioDefault"
+          id="flexRadioDefault2"
+          checked
+        />
+        <label className="form-check-label" htmlFor="flexRadioDefault2">
+          Default checked radio
+        </label>
+      </div>
     </>
-  )
+  );
 }
 
-export default FilterBtn
+export default FilterBtn;
